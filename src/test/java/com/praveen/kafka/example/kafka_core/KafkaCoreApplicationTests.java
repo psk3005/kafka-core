59b044ee9b352ff4f507187de8ba9e2922a1de69
Praveen Kithani <praveenk.kithani@gmail.com>
2025-12-19 14:25:32 +0530

    initial project setup

---
+++ src/test/java/com/praveen/kafka/example/kafka_core/KafkaCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.praveen.kafka.example.kafka_core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
